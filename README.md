ae8bd3e9ee2a81b24a9118a526c0b95d2a8015ff
unknown <john.gordos@outlook.com>
2023-06-14 09:32:12 -0600

    added classes for square and circle, completed prompts, wrote tests

--- README.md
+++ README.md
@@ -1,54 +1,15 @@
 # Logo Generator
 
-## Credit
+## Description
 
-REGEX For HEX Colors - https://www.geeksforgeeks.org/how-to-validate-hexadecimal-color-code-using-regular-expression/
-// Javascript program to validate
-// Hexadecimal Color Code using Regular Expression
+  <p align="center">
+    This application generates a logo (as an svg) based on terminal prompts that ask for shapes, colors, and 3 characters.
+  </p>
 
-// Function to validate the
-// hexadecimalColor_code
-function isValidHexaCode(str) {
-// Regex to check valid
-// hexadecimalColor_code
-let regex = new RegExp(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/);
+![Prompts To Generate Logo In The Terminal](lib/images/logo-generator-prompts.png?raw=true)
 
-    // if str
-    // is empty return false
-    if (str == null) {
-    	return "false";
-    }
+## Usage
 
-    // Return true if the str
-    // matched the ReGex
-    if (regex.test(str) == true) {
-    	return "true";
-    }
-    else {
-    	return "false";
-    }
+Pull the repo down to your machine. Make sure node, npm, and inquirer are installed. Then open the terminal and navigate to the main directory of the repo. Finally, run 'node index.js' in the terminal to initiate the prompts.
 
-}
-
-// Driver Code
-// Test Case 1:
-let str1 = "#1AFFa1";
-console.log(isValidHexaCode(str1));
-
-// Test Case 2:
-let str2 = "#F00";
-console.log(isValidHexaCode(str2));
-
-// Test Case 3:
-let str3 = "123456";
-console.log(isValidHexaCode(str3));
-
-// Test Case 4:
-let str4 = "#123abce";
-console.log(isValidHexaCode(str4));
-
-// Test Case 5:
-let str5 = "#afafah";
-console.log(isValidHexaCode(str5));
-
-// This code is contributed by Rahul Chauhan
+This video walks through using the app:
--- index.js
+++ index.js
@@ -1,22 +1,87 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const { Triangle } = require("./lib/shapes");
-function init() {
-  var svgTemplate = `<svg height="300" width="200" xmlns="http://www.w3.org/2000/svg">
+const { Triangle, Square, Circle } = require("./lib/shapes");
+var svgTemplate = `<svg height="200" width="300" xmlns="http://www.w3.org/2000/svg">
   -=shape=-
   -=text=-
   </svg>`;
-  // -=text=-prompts, calling supporting f() to build svg...
-  var myT = new Triangle(100, 100, "black");
-  var svgTriangle = myT.render();
-  const svgImage = svgTemplate.replace("-=shape=-", svgTriangle);
-  const svgContent = svgImage.replace("-=text=-", generateSVGText("PAIN"));
-  fs.writeFile("logo.svg", svgContent, logSVGCreationResult);
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message:
+          "What text do you want for your logo? Enter up to 3 characters.",
+        name: "logoText",
+        validate: (response) => {
+          return response.length == 3
+            ? true
+            : "The logo text must be 3 characters!";
+        },
+      },
+      {
+        type: "input",
+        message: "What color should the text be?",
+        name: "logoTextColor",
+      },
+      {
+        type: "list",
+        message: "Select a shape to use for your logo:",
+        choices: ["Square", "Triangle", "Circle"],
+        name: "logoShape",
+      },
+      {
+        type: "input",
+        message: "What color do you want your shape to be?",
+        name: "logoShapeColor",
+      },
+    ])
+    .then(handleLogoPrompts)
+    .catch((error) => {
+      console.log("A problem occurred!");
+      console.error(error);
+    });
 }
-function logSVGCreationResult(response) {
-  console.log(response);
+function handleLogoPrompts(response) {
+  const svgTemplateWithImage = svgTemplate.replace(
+    "-=shape=-",
+    returnShapeSVG(response.logoShape, response.logoShapeColor)
+  );
+  const svgFullContent = svgTemplateWithImage.replace(
+    "-=text=-",
+    returnTextSVG(response.logoText, response.logoTextColor)
+  );
+  fs.writeFile("logo.svg", svgFullContent, logSVGCreationResult);
 }
-function generateSVGText(text) {
-  return `<text x="150" y="100">${text}</text>`;
+function returnShapeSVG(logoShape, logoShapeColor) {
+  var shapeSVGResult = "";
+  switch (logoShape) {
+    case "Square":
+      var shape = new Square(logoShapeColor);
+      shapeSVGResult = shape.render();
+      break;
+    case "Triangle":
+      var shape = new Triangle(logoShapeColor);
+      shapeSVGResult = shape.render();
+      break;
+    case "Circle":
+      var shape = new Circle(logoShapeColor);
+      shapeSVGResult = shape.render();
+      break;
+    default:
+      /*A default value for shapeSVGResult is not assigned.
+      The responses from this prompt were collected using 
+      a list input, so only these values are expected AND 
+      must be selected.*/
+      break;
+  }
+  return shapeSVGResult;
+}
+function logSVGCreationResult() {
+  console.log("Generated logo.svg");
+}
+function returnTextSVG(logoText, logoTextColor) {
+  if (!logoTextColor) logoTextColor = "black";
+  return `<text x="140" y="100" fill="${logoTextColor}">${logoText}</text>`;
 }
 init();
